8b352bea0670068f4fbe734c74fd78bc8d65310c
Ajay Devarakonda <ajaydevarakonda150@gmail.com>
2025-09-10 23:03:40 +0530

    dad

--- src/App.jsx
+++ src/App.jsx
@@ -40,14 +40,11 @@ function App() {
   return (
     <>
     <BrowserRouter>
-      
-        
-      <div className='app'>
-        <div className='header'>
-          <Link to="/you" className={navIcon == "you"? "you1":"you2"} onClick={()=>setNavIcon("you")}><i className='fa fa-user'></i></Link>
-          <Link to="/" className={navIcon == "groups"? "you1":"you2"} onClick={()=>setNavIcon("groups")}><i className='fa fa-group'></i></Link>
-          <Link to="/menu" className={navIcon == "menu"? "you1":"you2"} onClick={()=>setNavIcon("menu")}><i className='fa fa-bars'></i></Link>
-        </div>
+      <div className='header'>
+        <Link to="/you" className={navIcon == "you"? "you1":"you2"} onClick={()=>setNavIcon("you")}><i className='fa fa-user'></i></Link>
+        <Link to="/" className={navIcon == "groups"? "you1":"you2"} onClick={()=>setNavIcon("groups")}><i className='fa fa-group'></i></Link>
+        <Link to="/menu" className={navIcon == "menu"? "you1":"you2"} onClick={()=>setNavIcon("menu")}><i className='fa fa-bars'></i></Link>
+      </div>
         <div className='body'>
           <Routes>
             <Route path='/you' element={<You setNavIcon={setNavIcon}></You>}></Route>
@@ -55,7 +52,6 @@ function App() {
             <Route path='/menu' element={<Menu setNavIcon={setNavIcon}></Menu>}></Route>
           </Routes>
         </div>
-      </div>
     </BrowserRouter>
     </>
   )
